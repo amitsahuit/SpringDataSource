9391cf30b0f0d9b51da5dc28c56ee8acb0f53e97
asahu23 <asahu23@INCNU4289RDH.CSCMWS.CSCMWS.COM>
2019-08-12 12:39:46 -0400

    Applying Spring

--- SpringDataSupport/src/com/amit/javaSpringDataSupport/JDBCDemo/Dao/JdbcDaoImpl.java
+++ SpringDataSupport/src/com/amit/javaSpringDataSupport/JDBCDemo/Dao/JdbcDaoImpl.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.amit.javaSpringDataSupport.JDBCDemo.module.Circle;
@@ -13,24 +16,42 @@ import com.amit.javaSpringDataSupport.JDBCDemo.module.Circle;
 @Component
 public class JdbcDaoImpl {
 
+	@Autowired
+	private DataSource datasource;
+	
+	public DataSource getDatasource() {
+		return datasource;
+	}
+
+	public void setDatasource(DataSource datasource) {
+		this.datasource = datasource;
+	}
+
 	public Circle getCircle(int circleId) {
 		
 		Connection conn = null;
 	
 		try {
+		
+		/*//Connection
 		String driver = "org.apache.derby.jdbc.ClientDriver";
 		Class.forName(driver).newInstance();
-		conn = DriverManager.getConnection("jdbc:derby://localhost:1527/db");
+		conn = DriverManager.getConnection("jdbc:derby://localhost:1527/db"); */
+		
+		conn = datasource.getConnection();		
+		
+		//Prepared statement
 		PreparedStatement ps = conn.prepareStatement("select * from circle where id=?");
 		ps.setInt(1, circleId);
 		
+		//Executing the query
 		Circle circle = null;
 		ResultSet rs = ps.executeQuery();
 		
+		//Passing throught he result set to get the output.
 		if(rs.next()) {
 			circle = new Circle(circleId, rs.getString("name"));
 		}
-		
 		rs.close();
 		ps.close();
 		
