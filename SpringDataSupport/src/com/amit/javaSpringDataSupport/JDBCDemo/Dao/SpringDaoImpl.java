2ebcd2c7799ca24836c48484ee7ba6c6098fc0f4
asahu23 <asahu23@INCNU4289RDH.CSCMWS.CSCMWS.COM>
2019-08-16 06:44:25 -0400

    usage of NamedParameterJdbcTemplate.

--- SpringDataSupport/src/com/amit/javaSpringDataSupport/JDBCDemo/Dao/SpringDaoImpl.java
+++ SpringDataSupport/src/com/amit/javaSpringDataSupport/JDBCDemo/Dao/SpringDaoImpl.java
@@ -11,6 +11,9 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 
 import com.amit.javaSpringDataSupport.JDBCDemo.module.Circle;
@@ -21,7 +24,8 @@ public class SpringDaoImpl {
 	//@Autowired
 	private DataSource datasource;
 	private JdbcTemplate jdbcTemplate; //= new JdbcTemplate();
-
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
 	public DataSource getDatasource() {
 		return datasource;
 	}
@@ -30,6 +34,7 @@ public class SpringDaoImpl {
 	public void setDatasource(DataSource dataSource) {
 		//this.dataSource = dataSource;
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 
 	public JdbcTemplate getJdbcTemplate() {
@@ -79,9 +84,17 @@ public class SpringDaoImpl {
 	  }
 	
 	//inserting into Circle
-	public void insertCircle(Circle circle) {
-		String sql= "INSERT INTO CIRCLE(ID, NAME) VALUES(?,?)";
-		jdbcTemplate.update(sql, new Object[] {circle.getId(), circle.getName()});
+	/*
+	 * public void insertCircle(Circle circle) { String sql=
+	 * "INSERT INTO CIRCLE(ID, NAME) VALUES(?,?)"; jdbcTemplate.update(sql, new
+	 * Object[] {circle.getId(), circle.getName()});}		
+	 */
+
+	//inserting into Circle via NamedParameterJdbcTemplate.
+	public void insertCircle(Circle circle) { 
+		String sql= "INSERT INTO CIRCLE(ID, NAME) VALUES(:id,:name)"; 
+		SqlParameterSource namedParameters = new MapSqlParameterSource("id", circle.getId()).addValue("name", circle.getName());
+		namedParameterJdbcTemplate.update(sql, namedParameters);
 	}
 	
 	//Creating table
--- SpringDataSupport/src/com/amit/javaSpringDataSupport/JDBCDemo/JdbcClient.java
+++ SpringDataSupport/src/com/amit/javaSpringDataSupport/JDBCDemo/JdbcClient.java
@@ -23,9 +23,9 @@ public class JdbcClient {
 		//System.out.println(dao.getCircleCount());
 		//System.out.println(dao.getCircleName(1));
 		//System.out.println(dao.getCircleForId(1).getName());
-		/*dao.insertCircle(new Circle(2, "Second Circle"));
-		System.out.println(dao.getAllCircles().size()); */
-		dao.createTriangleTable();
+		dao.insertCircle(new Circle(3, "Third Circle"));
+		System.out.println(dao.getAllCircles().size()); 
+		//dao.createTriangleTable();
 	}
 
 }
